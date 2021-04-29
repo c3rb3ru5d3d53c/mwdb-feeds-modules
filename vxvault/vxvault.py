cf83fde69174847d1d79ee15a49a7437da9222a8
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2021-04-29 07:10:45 -0300

    vxvault

--- vxvault/vxvault.py
+++ vxvault/vxvault.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import re
+import logging
 import requests
+
+log = logging.getLogger(__name__)
 
 __author__  = 'c3rb3ru5'
 __version__ = '1.0.0'
-
 
 class MWDBFeedsModule():
 
@@ -16,15 +18,27 @@ class MWDBFeedsModule():
     def __init__(self, config, mwdb):
         self.name    = 'vxvault'
         self.tag     = f'feed:{self.name}'
-        self.url     = 'http://vxvault.net/ViriList.php'
         self.enabled = self.startup(config)
+        self.url     = 'http://vxvault.net/ViriList.php'
         self.mwdb    = mwdb
 
     def startup(self, config):
-        options = ['count', 'pages']
+        options = ['enabled', 'limit', 'pages', 'verify_ssl']
         for option in options:
             if config.has_option(self.name, option) is False:
                 log.warning(f'{self.name} section is missing the {option} option')
                 return False
         self.config = config
-        return True
+        return self.config.getboolean(self.name, 'enabled')
+
+    def main(self) -> None:
+        regFiles = re.compile(r'files\/[a-fA-F0-9]{32}.zip')
+        page_limit = self.config.getint(self.name, 'limit')
+        pages = self.config.getint(self.name, 'pages')
+        count_limit = pages * page_limit
+        for i in range(0, pages *  page_limit, page_limit):
+            url = self.url + f'?s={i}&m={page_limit}'
+            print(url)
+            r = requests.get(url=url, verify=self.config.getboolean(self.name, 'verify_ssl'))
+            matches = re.findall(regFiles, r.content.decode('utf-8'))
+            print(matches)
